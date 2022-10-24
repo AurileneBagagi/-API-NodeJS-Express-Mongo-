416b5f0946c3cdaa8aa0203fe1406b41d89608b4
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2022-10-24 03:13:02 -0300

    primeira interação da api sem bd

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//Definindo
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (request, response) => {
+    response.send('OK');
+});
+
+app.listen(3000, () => console.log('running...'));
+
